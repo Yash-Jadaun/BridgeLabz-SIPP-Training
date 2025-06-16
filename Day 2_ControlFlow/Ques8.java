23fa377bfcf927b5d306352e231d7b2c941e6482
Yash Jadaun <yashjadaun2711@gmail.com>
2025-06-16 20:48:41 +0530

    [Yash Jadaun] Ques8

---
+++ Day 2_ControlFlow/Ques8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ques8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+
+        input.close();
+    }
+}
