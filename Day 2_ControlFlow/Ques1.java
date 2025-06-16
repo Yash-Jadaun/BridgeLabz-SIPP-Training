2503a482af30a83ecee6d5192fe5113a25daa8e1
Yash Jadaun <yashjadaun2711@gmail.com>
2025-06-16 19:54:06 +0530

    [Yash Jadaun] Ques1

---
+++ Day 2_ControlFlow/Ques1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Ques1{
+    
+    public static int function(int n){
+        if(n==1){
+            return 1;
+        }
+
+        return n*function(n-1);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+
+        int n= sc.nextInt();
+        int result = function(n);
+        System.out.println("The factorial of " + n + " is: " + result);
+        
+    }
+}
