ac2f2a2396f434ee13742b3bf45d7d2887350d7c
Yash Jadaun <yashjadaun2711@gmail.com>
2025-06-17 12:27:28 +0530

    [Yash Jadaun] Add checkNumberSign

---
+++ Day 3_ArrayPractice/Ques5.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Ques5 {
+
+    public static int checkNumberSign(int number) {
+        if (number < 0) {
+            return -1; // Negative
+        } else if (number > 0) {
+            return 1; // Positive
+        } else {
+            return 0; // Zero
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int num = scanner.nextInt();
+
+        int result = checkNumberSign(num);
+
+        if (result == -1) {
+            System.out.println("The number is negative.");
+        } else if (result == 1) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+
+        scanner.close();
+    }
+}
