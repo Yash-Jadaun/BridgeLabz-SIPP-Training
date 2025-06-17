c3f4f5f467cf1ce6886beede439198a610acf485
Yash Jadaun <yashjadaun2711@gmail.com>
2025-06-17 12:32:09 +0530

    [Yash Jadaun] Add findFactors

---
+++ Day 3_ArrayPractice/Ques9.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ques9 {
+
+    public static List<Integer> findFactors(int number) {
+        List<Integer> factors = new ArrayList<>();
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                factors.add(i);
+            }
+        }
+        return factors;
+    }
+
+    public static int sumFactors(List<Integer> factors) {
+        int sum = 0;
+        for (int factor : factors) {
+            sum += factor;
+        }
+        return sum;
+    }
+
+    public static long productFactors(List<Integer> factors) {
+        long product = 1;
+        for (int factor : factors) {
+            product *= factor;
+        }
+        return product;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int num = scanner.nextInt();
+
+        if (num <= 0) {
+            System.out.println("Please enter a positive integer.");
+            return;
+        }
+
+        List<Integer> factors = findFactors(num);
+        System.out.println("Factors of " + num + ": " + factors);
+        System.out.println("Sum of factors: " + sumFactors(factors));
+        System.out.println("Product of factors: " + productFactors(factors));
+
+        scanner.close();
+    }
+}
