54842a29ece1d86f701cc1bc316f49aa14ab641c
Yash Jadaun <yashjadaun2711@gmail.com>
2025-06-19 09:47:00 +0530

    [Yash Jadaun] Add FindLengthOfString

---
+++ Day4_Strings/Ques7.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Ques7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String inputString = scanner.nextLine();
+        System.out.print("Enter an index to access: ");
+        int index = scanner.nextInt();
+
+        try {
+            char character = inputString.charAt(index); // Accessing an invalid index
+            System.out.println("Character at index " + index + ": " + character);
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("StringIndexOutOfBoundsException caught.");
+        } finally {
+            scanner.close();
+        }
+    }
+}
