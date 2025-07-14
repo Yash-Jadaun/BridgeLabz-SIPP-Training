4ed90c5581fdb07c5cc6142fe09d99762eaf1f60
Yash Jadaun <yashjadaun2711@gmail.com>
2025-07-14 18:43:18 +0530

    [Yash Jadaun] StringBuilder

--- DataStructure/LinearandBinarySearch/append.java
+++
@@ -1,9 +0,0 @@
-public class append {
-    public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 10; i++) {
-        sb.append(i);
-}
-
-    }
-}
