973bec3290b2d2e341da554decee7c7c35f68478
Yash Jadaun <yashjadaun2711@gmail.com>
2025-07-14 18:36:21 +0530

    commit

---
+++ DataStructure/LinearandBinarySearch/append.java
@@ -0,0 +1,9 @@
+public class append {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+        sb.append(i);
+}
+
+    }
+}
