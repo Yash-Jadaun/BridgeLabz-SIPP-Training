4cc8f9d7e5edfe514dada83f8694730b6f8d79ba
Yash Jadaun <yashjadaun2711@gmail.com>
2025-07-15 10:28:28 +0530

    [Yash Jadaun] Add ReadFile

---
+++ DataStructure/LinearandBinarySearch/FileReader/ReadFile.java
@@ -0,0 +1,16 @@
+package DataStructure.LinearandBinarySearch.FileReader;
+
+import java.io.*;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("file.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
