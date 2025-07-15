f684d883821381537463745a3464fdad95b23484
Yash Jadaun <yashjadaun2711@gmail.com>
2025-07-15 12:59:51 +0530

    [Yash Jadaun] Add InsertionSortEmployeeId

---
+++ DataStructure/SortingAlgo/BubbleSort/InsertionSortEmployeeIDs.java
@@ -0,0 +1,23 @@
+package DataStructure.SortingAlgo.BubbleSort;
+
+public class InsertionSortEmployeeIDs {
+    public static void main(String[] args) {
+        int[] employeeIDs = {103, 101, 105, 102, 104};
+
+        for (int i = 1; i < employeeIDs.length; i++) {
+            int key = employeeIDs[i];
+            int j = i - 1;
+
+            while (j >= 0 && employeeIDs[j] > key) {
+                employeeIDs[j + 1] = employeeIDs[j];
+                j--;
+            }
+            employeeIDs[j + 1] = key;
+        }
+
+        System.out.println("Sorted Employee IDs:");
+        for (int id : employeeIDs) {
+            System.out.print(id + " ");
+        }
+    }
+}
