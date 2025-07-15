4d795f52820cc8fa06b1a1dcf5aabc71c6c7a607
Yash Jadaun <yashjadaun2711@gmail.com>
2025-07-15 10:12:52 +0530

    [Yash Jadaun] Add ReverseString

---
+++ DataStructure/LinearandBinarySearch/ReverseString.java
@@ -0,0 +1,13 @@
+package DataStructure.LinearandBinarySearch;
+import java.util.*;
+
+public class ReverseString {
+    public static String reverse(String input) {
+        StringBuilder sb = new StringBuilder(input);
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse("hello")); // Output: olleh
+    }
+}
