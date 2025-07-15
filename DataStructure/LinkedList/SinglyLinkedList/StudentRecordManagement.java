a7647b1bb61a0f3f92b21e3ba62269907ed24efa
Yash Jadaun <yashjadaun2711@gmail.com>
2025-07-15 12:38:42 +0530

    [Yash Jadaun] Add Browser

--- DataStructure/LinkedList/SinglyLinkedList/StudentRecordManagement.java
+++
@@ -1,5 +0,0 @@
-package DataStructure.LinkedList.SinglyLinkedList;
-
-public class StudentRecordManagement {
-    
-}
---
+++ workshop/BrowserQues.java
@@ -0,0 +1,7 @@
+package workshop;
+
+public class BrowserQues {
+    public static void main(String[] args) {
+        
+    }
+}
